e946b438abf8dcd2df7ac747af88be17c4043ce4
joeygabin <joeygabin44@gmail.com>
2021-11-11 22:32:05 +0800

    new8

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -65,6 +66,7 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
+    <RestaurantsContextProvider>
     <NavigationContainer>
     <Tab.Navigator
              screenOptions={createScreenOptions}
@@ -78,6 +80,7 @@ export default function App() {
             <Tab.Screen name="Settings" component={Settings} />
           </Tab.Navigator>
     </NavigationContainer>
+    </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
   </>
