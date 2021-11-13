b6b921396511ba2d93179654834e9c760ec18dc3
joeygabin <joeygabin44@gmail.com>
2021-11-13 21:48:04 +0800

    new9

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { LocationContextProvider } from "./src/services/location/location.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -66,6 +67,7 @@ export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
+    <LocationContextProvider>
     <RestaurantsContextProvider>
     <NavigationContainer>
     <Tab.Navigator
@@ -81,6 +83,7 @@ export default function App() {
           </Tab.Navigator>
     </NavigationContainer>
     </RestaurantsContextProvider>
+    </LocationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
   </>
