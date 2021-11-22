0f2bcc67d9162d6989c547688ae1b05baeb89e22
joeygabin <joeygabin44@gmail.com>
2021-11-22 21:21:57 +0800

    login ins newe input

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import React from 'react';
-
+import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
 import {
   useFonts as useOswald,
@@ -13,9 +13,23 @@ import { theme } from "./src/infrastructure/theme";
 
 import { Navigation } from "./src/infrastructure/navigation";
 
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+
+// video #50
+const firebaseConfig = {
+  apiKey: "AIzaSyAVUbHlCSwNFSY-7ut_a9bDGXyFv_A6inU",
+  authDomain: "mealstogo-fb620.firebaseapp.com",
+  projectId: "mealstogo-fb620",
+  storageBucket: "mealstogo-fb620.appspot.com",
+  messagingSenderId: "170557914939",
+  appId: "1:170557914939:web:1deff6020687cdc546811e", 
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
 
@@ -31,16 +45,16 @@ export default function App() {
     return null;
   }
 
+
+
   return (
     <>
     <ThemeProvider theme={theme}>
-    <FavouritesContextProvider>
-    <LocationContextProvider>
-    <RestaurantsContextProvider>
+    <AuthenticationContextProvider>
+   
     <Navigation />
-    </RestaurantsContextProvider>
-    </LocationContextProvider>
-    </FavouritesContextProvider>
+    
+    </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
   </>
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,14 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React, { useContext } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 
 
 import { SafeArea } from "../../components/utility/safe-area.component";
 
 import { RestaurantsNavigator } from "./restaurants.navigator";
 import { MapScreen } from "../../features/map/screens/map.screen";
+
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
+
+import { RestaurantsContextProvider } from "../../services/restaurants/restaurants.context";
+import { LocationContextProvider } from "../../services/location/location.context";
+import { FavouritesContextProvider } from "../../services/favourites/favourites.context";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,11 +24,15 @@ const TAB_ICON = {
   Settings: "md-settings",
 };
 
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
+const Settings = () => {
+  const { onLogout } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+      <Button title="logout" onPress={() => onLogout()} />
+    </SafeArea>
+  );
+};
 
 
 const createScreenOptions = ({ route }) => {
@@ -35,18 +45,23 @@ const createScreenOptions = ({ route }) => {
 };
 
 export const AppNavigator = () => (
-  <NavigationContainer>
-    <Tab.Navigator
-      screenOptions={createScreenOptions}
-      tabBarOptions={{
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
-      }}
-    >
-      
-       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <FavouritesContextProvider>
+    <LocationContextProvider>
+      <RestaurantsContextProvider>
+  
+  <Tab.Navigator
+  screenOptions={createScreenOptions}
+  tabBarOptions={{
+    activeTintColor: "tomato",
+    inactiveTintColor: "gray",
+  }}
+>
+  <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+  <Tab.Screen name="Map" component={MapScreen} />
+  <Tab.Screen name="Settings" component={Settings} />
+</Tab.Navigator>
+</RestaurantsContextProvider>
+    </LocationContextProvider>
+  </FavouritesContextProvider>
+ 
 );
